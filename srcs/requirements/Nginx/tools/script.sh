f56a44ef107c69738f999403b540ca0a6acf149a
M4rrs <almasnorazman2803@outlook.com>
2023-11-08 18:45:44 +0800

    mariadb container

--- srcs/requirements/Nginx/tools/script.sh
+++ srcs/requirements/Nginx/tools/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Generate a self-signed certificate
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=MY/ST=Selangor/L=Sunway/O=42 Malaysia/OU=Student/CN=Mars"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out $CERTS_ -subj "/C=MY/ST=Selangor/L=Sunway/O=42 Malaysia/OU=Student/CN=Mars"
 
 # Prevents nginx from running in the background as a daemon process
 # Considered as best practice for Docker containers, one container = one process.
