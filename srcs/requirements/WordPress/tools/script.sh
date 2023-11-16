1204b50d0d44ed0190d5037250212c4f99b4a3ae
M4rrs <almasnorazman2803@outlook.com>
2023-11-16 17:03:50 +0800

    wp install

--- srcs/requirements/WordPress/tools/script.sh
+++ srcs/requirements/WordPress/tools/script.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-cd /var/www/html
+if [ -f ./wp-config.php ]; then
+	echo "WordPress already installed"
+else
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	wp core download
+fi
