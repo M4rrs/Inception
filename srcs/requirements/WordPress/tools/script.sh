dc26bf515caf902eddcb708a36500003a47ef631
m4rrs <almasnorazman2803@gmail.com>
2023-11-22 17:34:31 +0800

    wp wip

--- srcs/requirements/WordPress/tools/script.sh
+++ srcs/requirements/WordPress/tools/script.sh
@@ -10,7 +10,11 @@ else
 	wp core download --allow-root	
 
 	cp wp-config-sample.php wp-config.php
-	
+	sed -i 's/database_name_here/$WP_DATABASE/1' wp-config.php
+	sed -i 's/username_here/$MYSQL_USER/1' wp-config.php
+	sed -i 's/password_here/$MYSQL_PASSWORD/1' wp-config.php
+	sed -i 's/localhost/$WP_LOCALHOST/1' wp-config.php
+
 fi
 
 tail -f /dev/null
