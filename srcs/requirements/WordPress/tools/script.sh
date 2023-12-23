c787eaa659a0814a4158deae8ed9d17c0b3fe31c
m4rrs <almasnorazman2803@gmail.com>
2023-12-23 17:14:30 +0800

    updated 50-server.cnf. Still unsure why database not connecting

--- srcs/requirements/WordPress/tools/script.sh
+++ srcs/requirements/WordPress/tools/script.sh
@@ -10,12 +10,13 @@ else
 	wp core download --allow-root	
 
 	cp wp-config-sample.php wp-config.php
-	sed -i 's/database_name_here/$WP_DATABASE/1' wp-config.php
+	sed -i 's/database_name_here/$MYSQL_DATABASE/1' wp-config.php
 	sed -i 's/username_here/$MYSQL_USER/1' wp-config.php
 	sed -i 's/password_here/$MYSQL_PASSWORD/1' wp-config.php
 	sed -i 's/localhost/$WP_LOCALHOST/1' wp-config.php
 
 	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_name=$WP_ADMIN --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=$WP_EMAIL --allow-root
+	wp user create $WP_USER	$WP_USER_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
 fi
 
 sed -i 's|/run/php/php7.3-fpm.sock|9000|' /etc/php/7.3/fpm/pool.d/www.conf
