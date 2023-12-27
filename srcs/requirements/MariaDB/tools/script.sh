eec57a877e24100f6f537f35bbd1939cac4d37cd
m4rrs <almasnorazman2803@gmail.com>
2023-12-27 18:57:38 +0800

    ??????

--- srcs/requirements/MariaDB/tools/script.sh
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
-# service mysql start
-
-# # echo "FLUSH PRIVILEGES;" | mysql
-# echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql
-# echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
-# echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" | mysql
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql
-# echo "FLUSH PRIVILEGES;" | mysql
-
-# kill $(cat /var/run/mysqld/mysqld.pid)
-
-# mysqld
+cd /mariadb_dir
 
 echo "USE mysql;
 FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 FLUSH PRIVILEGES;" > /tmp/script.sql
 
 #Initial db setup
 mysql_install_db --user=mysql
 mysqld --user=mysql --bootstrap < /tmp/script.sql
 mysqld_safe --user=mysql
+
+sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+# tail -f /dev/null
